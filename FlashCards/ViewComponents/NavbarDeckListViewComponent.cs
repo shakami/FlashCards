fe41bdb7cf8f70bedf231e2d4a5b4d928070410f
Sepehr <Sepehr@DESKTOP-J432CR0>
2020-01-05 18:31:01 -0800

    added deck list navbar

---
+++ FlashCards/ViewComponents/NavbarDeckListViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using FlashCards.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlashCards.ViewComponents
+{
+    public class NavbarDeckListViewComponent : ViewComponent
+    {
+        private readonly IFlashCardDataService _flashCardDataService;
+
+        public NavbarDeckListViewComponent(IFlashCardDataService flashCardDataService)
+        {
+            _flashCardDataService = flashCardDataService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _flashCardDataService.GetAllDecks();
+            return View(model);
+        }
+    }
+}
